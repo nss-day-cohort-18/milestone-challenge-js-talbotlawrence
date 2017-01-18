d1d0875fef512c81b90db3791ce8dbab89b79b3a
Talbot <talbotlawrence75@gmail.com>
2017-01-17 20:11:49 -0600

    added a little more structure to html and js files

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
+//tree function
+//build in console.log
+//the user can either then just press the enter key (as long as the cursor is in one of the input fields)
+//or click a button that is labeled "Grow your tree"
+//event listener to the button, as well as an event listener for the enter/return key.
+//http://www.w3schools.com/js/js_htmldom_eventlistener.asp
+//display an alert stating that both fields must have a value.
+
+// sss*sss
+// ss***ss
+// s*****s
+// *******
+
 function tree() {
 
 }
 
-console.log("");
 {
-	height: 7,
-	leaf: "*",
+	height: height-input,
+	leaf: character-input,
 
 }
 
-// add += 2
-alert("both fields must have a value");
+for (var i = 0; i < height-input.length; i++) {
+	for (var j = 0; j < Things.length; j++) {
+		Things[j]
+	}
+}
+
+document.getElementById("myBtn").addEventListener("click", displayDate);  //an example
+
+// add += 2 maybe
+alert("Both fields must have a value");
